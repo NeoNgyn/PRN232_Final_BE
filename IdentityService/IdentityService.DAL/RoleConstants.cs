9bf357d228604c373103d1ecb4d5682f4f6e51a2
NhanNPSE184696 <nguyenphucnhan2004@gmail.com>
2025-11-14 22:33:53 +0700

    refactor(files): simplify method signatures and improve parameter handling in FilesController refactor(submission): update CreateSubmission method to remove unnecessary FromForm attribute fix(auth): change default user role to ExaminerRoleId in AuthService chore(role): update role constants to include new roles and modify Admin and DefaultUser role IDs

--- IdentityService/IdentityService.DAL/RoleConstants.cs
+++ IdentityService/IdentityService.DAL/RoleConstants.cs
@@ -5,8 +5,10 @@ namespace IdentityService.DAL
     public static class RoleConstants
     {
         // Default role IDs - should match your database
-        public static readonly Guid AdminRoleId = Guid.Parse("11111111-1111-1111-1111-111111111111");
-        public static readonly Guid LecturerRoleId = Guid.Parse("22222222-2222-2222-2222-222222222222");
+        public static readonly Guid AdminRoleId = Guid.Parse("a2eaee3a-8e34-400e-a2d5-1c0ac26e3124");
+        public static readonly Guid ManagerRoleId = Guid.Parse("db45356f-1f54-432c-877a-965aa64e593d");
+        public static readonly Guid ModeratorRoleId = Guid.Parse("b34ee72e-7d5e-4cc1-aaa3-27d53d86f497");
+        public static readonly Guid ExaminerRoleId = Guid.Parse("907a702d-5b01-4072-84b4-72de619568f9");
         public static readonly Guid DefaultUserRoleId = Guid.Parse("33333333-3333-3333-3333-333333333333");
 
         // Role names
